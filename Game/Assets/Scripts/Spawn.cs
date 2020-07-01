5f5603ecf05e65ab5fecc85c024b407fe5fdf21a
Theodora Tosheva <theodora@uber.com>
2020-07-01 18:26:38 +0300

    Add Spawn zombie script

---
+++ Game/Assets/Scripts/Spawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    // The Bullet Prefab
+    public GameObject zombiePrefab;
+
+    public float interval = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("StartSpawn", 0, interval);
+    }
+
+    void StartSpawn() {
+        Instantiate(zombiePrefab, transform.position, Quaternion.identity);
+    }
+}
