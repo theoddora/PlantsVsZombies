0773c3df8682c055407ed956b151c1d7f522af56
Theodora Tosheva <theodora@uber.com>
2020-07-01 12:57:16 +0300

    Background and Sunflower

---
+++ Game/Assets/Scripts/HealthGlobal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HealthGlobal : MonoBehaviour
+{
+    // Current Health
+    [SerializeField]
+    int cur = 5;
+
+    public void doDamage(int n) {
+        // Subtract damage from current health
+        cur -= n;
+
+         // Destroy if died
+        if (cur <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
